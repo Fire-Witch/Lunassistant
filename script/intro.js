b67344e1d1bb820e65cf03b28dac59c7ea6889e7
Fire-Witch <74001300+Fire-Witch@users.noreply.github.com>
2020-11-08 17:56:31 +0100

    First animation done

--- script/intro.js
+++ script/intro.js
@@ -1,1 +1,8 @@
-document.getElementById('imgLune').style.animation = "luneGrow 1s";
+document.getElementsByTagName("body")[0].style.overflowY = "hidden"; 
+document.getElementById('imgLune').style.animation = "luneStart 1s";
+setTimeout(function(){document.getElementById('lunaFace').style.animation = "faceGoLeftStart 1s";}, 1000);  
+setTimeout(function(){document.getElementById('lunaFace').style.width = "20%"}, 2000);
+setTimeout(function(){document.getElementById('lunaFace').style.marginLeft = "20%"}, 2000);
+setTimeout(function(){document.getElementById('titreLuna').style.animation = "titreAppearStart 0.5s";}, 2000); 
+setTimeout(function(){document.getElementById('titreLuna').style.opacity = "100%";}, 2500); 
+setTimeout(function(){document.getElementsByTagName("body")[0].style.overflowY = "";}, 2500);  
