9ee81c1b4e0b6efe385bb5c67f9396fb8e6e5f14
Fire-Witch <74001300+Fire-Witch@users.noreply.github.com>
2020-11-09 18:27:51 +0100

    Add placeholder button and finish start animation

--- script/intro.js
+++ script/intro.js
@@ -1,17 +1,38 @@
 //animation de l'intro
 //on cache la scrollbar
-document.getElementsByTagName("body")[0].style.overflowY = "hidden"; 
+document.getElementsByTagName("body")[0].style.overflowY = "hidden";
 //on appelle l'animation de la lune qui rétrécit 
 document.getElementById('imgLune').style.animation = "luneStart 1s";
 //on appelle l'animation qui met Luna en haut
-setTimeout(function(){document.getElementById('lunaFace').style.animation = "faceGoLeftStart 1s";}, 1000);  
+setTimeout(function () { document.getElementById('lunaFace').style.animation = "faceGoLeftStart 1s"; }, 1000);
 //on fixe la taille de Luna
-setTimeout(function(){document.getElementById('lunaFace').style.width = "20%"}, 2000);
+setTimeout(function () { document.getElementById('lunaFace').style.width = "20%" }, 2000);
 //on fixe la marge gauche de Luna
-setTimeout(function(){document.getElementById('lunaFace').style.marginLeft = "20%"}, 2000);
+setTimeout(function () { document.getElementById('lunaFace').style.marginLeft = "20%" }, 2000);
 //on appelle l'animation qui fait apparaitre le titre
-setTimeout(function(){document.getElementById('titreLuna').style.animation = "titreAppearStart 0.5s";}, 2000); 
+setTimeout(function () { document.getElementById('titreLuna').style.animation = "titreAppearStart 0.5s"; }, 2000);
 //on fixe l'opacité du titre
-setTimeout(function(){document.getElementById('titreLuna').style.opacity = "100%";}, 2500); 
+setTimeout(function () { document.getElementById('titreLuna').style.opacity = "100%"; }, 2500);
+setTimeout(function () { document.getElementsByTagName("body")[0].style.overflowY = ""; }, 2500);
+
+setTimeout(function () {
+    var all = document.getElementsByClassName('buttonAction');
+    for (var i = 0; i < all.length; i++) {
+        all[i].style.animation = 'buttonAppear 0.5s';
+    }
+}, 2100);
+setTimeout(function () {
+    var all = document.getElementsByClassName('buttonAction');
+    for (var i = 0; i < all.length; i++) {
+        all[i].style.marginTop = "3%";
+    }
+}, 2100);
+setTimeout(function () {
+    var all = document.getElementsByClassName('buttonAction');
+    for (var i = 0; i < all.length; i++) {
+        all[i].style.opacity = "100%";
+    }
+}, 2100);
+
 //on montre la scrollbar
-setTimeout(function(){document.getElementsByTagName("body")[0].style.overflowY = "";}, 2500);  
+setTimeout(function () { document.getElementsByTagName("body")[0].style.overflowY = ""; }, 2600);  
